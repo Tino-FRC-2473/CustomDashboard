a2704c59e5d76e7ec2449eef33469ed57528e67b
Deep Sethi <deeptanshu.146@gmail.com>
2016-11-22 08:46:41 -0800

    Simple GUI Test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class Main extends Application {
 
@@ -12,9 +15,18 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Stage window = primaryStage;
+
+        Label encoder_tr_label = new Label("Encoder value: ");
+
+        HBox layout = new HBox(20);
+
+        layout.getChildren().addAll(encoder_tr_label);
+
+        Scene scene = new Scene(layout, 300, 275);
+
+        primaryStage.setTitle("");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
