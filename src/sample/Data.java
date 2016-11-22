170edb10c43d78b224144cb59df931fc6e4ac5c6
Deep Sethi <deeptanshu.146@gmail.com>
2016-11-22 14:53:40 -0800

    Data syncronization enabled

---
+++ src/sample/Data.java
@@ -0,0 +1,25 @@
+package sample;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Data {
+    public static void data() {
+        try (Socket s = new Socket("RoboRIO-2473-FRC.local", 2005)) {
+            InputStream in = s.getInputStream();
+            Scanner scan = new Scanner(in);
+
+                System.out.print("connected");
+
+                while (scan.hasNextLine()) {
+                    System.out.println(scan.nextLine());
+                }
+
+
+            } catch(IOException e) {
+                System.out.println(e.getMessage());
+            }
+    }
+}
