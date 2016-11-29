1f490eb74bc3d727cd0a69f3d0ec8239120325c8
Deep Sethi <deeptanshu.146@gmail.com>
2016-11-29 10:13:03 -0800

    Minor edits

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,28 +5,42 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.stage.Window;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        /*Root and window*/
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
         Stage window = primaryStage;
+        window.initStyle(StageStyle.UNDECORATED);
 
-        Label encoder_tr_label = new Label("Encoder value: ");
+        /*Parent layout*/
+        Pane pane = new Pane();
 
-        HBox layout = new HBox(20);
+        /*Main Screen Scene*/
+        Scene main = new Scene(pane, 1280, 1024, Color.BLACK);
 
-        layout.getChildren().addAll(encoder_tr_label);
+        /*Top green bar*/
+        Rectangle top_bar = new Rectangle();
+        top_bar.setFill(Color.web("rgba(29,255,42,1.0)"));
+        top_bar.setHeight(24);
+        top_bar.setWidth(main.getWidth());
+        top_bar.setLayoutX(0);
+        top_bar.setLayoutY(0);
+        pane.getChildren().addAll(top_bar);
 
-        Scene scene = new Scene(layout, 300, 275);
-
-        primaryStage.setTitle("");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(main);
+        primaryStage.setTitle("FRC " + Constants.TEAM_NUMBER + " Dashboard");
         primaryStage.show();
     }
 
