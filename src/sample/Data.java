a0c4b84676fb84244ac11b8adfc3e7645a2ae731
Deep Sethi <deeptanshu.146@gmail.com>
2016-11-29 10:12:36 -0800

    Minor UI edits

--- src/sample/Data.java
+++ src/sample/Data.java
@@ -6,7 +6,8 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Data {
-    public static void data() {
+    public static String getData() {
+        String returner = "";
         try (Socket s = new Socket("RoboRIO-2473-FRC.local", 2005)) {
             InputStream in = s.getInputStream();
             Scanner scan = new Scanner(in);
@@ -14,12 +15,14 @@ public class Data {
                 System.out.print("connected");
 
                 while (scan.hasNextLine()) {
-                    System.out.println(scan.nextLine());
+                    System.out.println(scan.nextLine()); //Testing
+                    returner += scan.nextLine();
                 }
 
 
             } catch(IOException e) {
                 System.out.println(e.getMessage());
             }
+            return returner;
     }
 }
