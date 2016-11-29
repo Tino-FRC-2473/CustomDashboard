602bde3f51b46e81d9e5b73bfb508c480e015de3
kashyappanda <kashyap.panda@gmail.com>
2016-11-28 20:33:34 -0800

    Create a map of robot values
    Takes the gyro, throttle, and encoder values from the robot and puts it
    into a map.

--- src/sample/Data.java
+++ src/sample/Data.java
@@ -3,23 +3,38 @@ package sample;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Data {
     public static void data() {
-        try (Socket s = new Socket("RoboRIO-2473-FRC.local", 2005)) {
-            InputStream in = s.getInputStream();
-            Scanner scan = new Scanner(in);
+        Map<String, Double> values = new HashMap<>(4);
+        values.put("THROTTLE", null);
+        values.put("GYRO", null);
+        values.put("LEFT_ENCODER", null);
+        values.put("RIGHT_ENCODER", null);
 
-                System.out.print("connected");
+        try (Socket s = new Socket("RoboRIO-2473-FRC.local", 2005);
+             InputStream in = s.getInputStream();
+             Scanner scan = new Scanner(in)) {
 
-                while (scan.hasNextLine()) {
-                    System.out.println(scan.nextLine());
+            System.out.print("connected");
+
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                if (line.contains("THROTTLE")) {
+                    values.put("THROTTLE", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
+                } else if (line.contains("GYRO")) {
+                    values.put("GYRO", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
+                } else if (line.contains("LEFT_ENC")) {
+                    values.put("LEFT_ENCODER", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
+                } else if (line.contains("RIGHT_ENC")) {
+                    values.put("RIGHT_ENCODER", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
                 }
-
-
-            } catch(IOException e) {
-                System.out.println(e.getMessage());
             }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
