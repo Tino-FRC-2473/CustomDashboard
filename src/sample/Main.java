dc5a572edaef39d257c9af975c6dd098456f98ed
kashyappanda <kashyap.panda@gmail.com>
2016-12-05 21:02:06 -0800

    Created a draggable window and editable text

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.binding.StringBinding;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,27 +9,34 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.Window;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private  double yOffset = 0;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage)throws Exception{
         /*Root and window*/
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Stage window = primaryStage;
-        window.initStyle(StageStyle.UNDECORATED);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
 
         /*Parent layout*/
-        Pane pane = new Pane();
+        Pane pane = new VBox();
 
         /*Main Screen Scene*/
-        Scene main = new Scene(pane, 1280, 1024, Color.BLACK);
+        Scene main = new Scene(pane, 800, 600, Color.BLACK);
 
         /*Top green bar*/
         Rectangle top_bar = new Rectangle();
@@ -39,9 +47,28 @@ public class Main extends Application {
         top_bar.setLayoutY(0);
         pane.getChildren().addAll(top_bar);
 
+        //Text testing stuff
+        Text text = new Text(6400, 512, "TESTING");
+        text.setFont(new Font(40));
+        text.setFill(new Color(1.0, 1.0, 1.0, 1.0));
+//        text.textProperty().bind(StringBinding.stringExpression());
+        pane.getChildren().add(text);
+
         primaryStage.setScene(main);
         primaryStage.setTitle("FRC " + Constants.TEAM_NUMBER + " Dashboard");
         primaryStage.show();
+
+
+        pane.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+            text.setText("Press");
+        });
+        pane.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+            text.setText("Drag");
+        });
     }
 
 
@@ -49,3 +76,12 @@ public class Main extends Application {
         launch(args);
     }
 }
+
+/*
+                                        Works Cited
+Penzov, Peter. “How to Drag Undecorated Window.” Javafx 2 - How to Drag Undecorated Window - Stack Overflow,
+    StackExchange, 11 Aug. 2013, stackoverflow.com/questions/18173956/how-to-drag-undecorated-window.
+ */
+
+
+
