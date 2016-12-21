a84f9dc6f4fd1228a1b5cbece592da89e37dddf3
kashyappanda <kashyap.panda@gmail.com>
2016-12-21 14:49:32 -0800

    Modified Data.java

--- src/sample/Data.java
+++ src/sample/Data.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Data {
     private static Map<String, Double> values = FXCollections.observableHashMap();
 
-    public static void refresh() {
+    public static void connect() {
         values.put("THROTTLE", null);
         values.put("GYRO", null);
         values.put("LEFT_ENCODER", null);
@@ -21,24 +21,29 @@ public class Data {
              InputStream in = s.getInputStream();
              Scanner scan = new Scanner(in)) {
 
-            System.out.print("connected");
+            System.out.println("connected");
 
             //Gets values from the robot and puts it into a map
             while (scan.hasNextLine()) {
-                String line = scan.nextLine();
-                if (line.contains("THROTTLE")) {
-                    values.put("THROTTLE", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
-                } else if (line.contains("GYRO")) {
-                    values.put("GYRO", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
-                } else if (line.contains("LEFT_ENC")) {
-                    values.put("LEFT_ENCODER", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
-                } else if (line.contains("RIGHT_ENC")) {
-                    values.put("RIGHT_ENCODER", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
-                }
+//                String line = scan.nextLine();
+//                if (line.contains("THROTTLE")) {
+//                    values.put("THROTTLE", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
+//                } else if (line.contains("GYRO")) {
+//                    values.put("GYRO", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
+//                } else if (line.contains("LEFT_ENC")) {
+//                    values.put("LEFT_ENCODER", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
+//                } else if (line.contains("RIGHT_ENC")) {
+//                    values.put("RIGHT_ENCODER", Double.parseDouble(line.substring(line.lastIndexOf(":") + 1)));
+//                }
+                System.out.println(scan.nextLine());
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void clear() {
+        values.clear();
     }
 
     public static Map<String, Double> getValues() {
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,9 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -19,24 +11,6 @@ public class Main extends Application {
 
     private static double xOffset = 0;
     private static double yOffset = 0;
-
-    @Override
-    public void start(Stage primaryStage)throws Exception{
-        /*Root and window*/
-        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
-
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-
-        Scene scene = new Scene(root, 536, 400);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        root.setOnMouseDragged(event -> {
-            primaryStage.setX(event.getScreenX() - xOffset);
-            primaryStage.setY(event.getScreenY() - yOffset);
-        });
-    }
-
 
     public static void main(String[] args) {
         launch(args);
@@ -56,6 +30,23 @@ public class Main extends Application {
 
     public static void setyOffset(double offset) {
         yOffset = offset;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        /*Root and window*/
+        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        Scene scene = new Scene(root, 536, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
     }
 }
 
