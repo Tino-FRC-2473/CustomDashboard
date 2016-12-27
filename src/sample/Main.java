5f19905546fb93a508f323a80f0187e6fabbb0fd
Deep Sethi <deeptanshu.146@gmail.com>
2016-12-27 15:01:18 -0800

    test runs successful, maps updated from robot

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,11 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,20 +19,32 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main extends Application {
 
     private static double xOffset = 0;
     private static double yOffset = 0;
-
+    NetworkingThread networkingThread;
     @Override
     public void start(Stage primaryStage)throws Exception{
+        Map<String, String> map = new HashMap<>();
+
+        networkingThread = new NetworkingThread(2005, map);
+        networkingThread.start();
         /*Root and window*/
         Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
 
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
         Scene scene = new Scene(root, 536, 400);
+        scene.getStylesheets().add("stylesheet.css");
+        Controller control = new Controller(scene, map);
+
         primaryStage.setScene(scene);
         primaryStage.show();
 
@@ -35,11 +52,23 @@ public class Main extends Application {
             primaryStage.setX(event.getScreenX() - xOffset);
             primaryStage.setY(event.getScreenY() - yOffset);
         });
+
+        Timeline time = new Timeline(new KeyFrame(new Duration(100), e -> {
+                control.update();
+            }
+        ));
+        time.setCycleCount(Timeline.INDEFINITE);
+        time.play();
+//        control.addSystem("Memes");
     }
 
+    @Override
+    public void stop() {
+        networkingThread.quit();
+    }
 
     public static void main(String[] args) {
-        launch(args);
+//        launch(args);
     }
 
     public static double getxOffset() {
