efda0d668abf578fc59caa67ab9cbb509586b3f6
harmhe <he.harmony@gmail.com>
2017-10-28 15:52:48 -0700

    Created TabPane + 3 tabs, TESTED

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,18 +2,47 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("application.fxml"));
+//      Parent root = FXMLLoader.load(getClass().getResource("application.fxml"));
+    	Group root = new Group();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Rectangle2D screenBalance = Screen.getPrimary().getVisualBounds(); //for getting screen dimensions
+        primaryStage.setScene(new Scene(root, screenBalance.getWidth(), 600)); 
+        
+        TabPane tabPane = new TabPane();
+        Tab driverTab = new Tab();
+        driverTab.setText("Driver");
+        driverTab.setContent(new Rectangle(2000, 600, Color.LIGHTSTEELBLUE));
+        tabPane.getTabs().add(driverTab);
+        
+        Tab swTab = new Tab();
+        swTab.setText("Software");
+        swTab.setContent(new Rectangle(2000, 600, Color.LIGHTSTEELBLUE));
+        tabPane.getTabs().add(swTab);
+        
+        Tab graphTab = new Tab();
+        graphTab.setText("Graphs");
+        graphTab.setContent(new Rectangle(2000, 600, Color.LIGHTSTEELBLUE));
+        tabPane.getTabs().add(graphTab);
+        
+
+        root.getChildren().add(tabPane);
         primaryStage.show();
+       
     }
 
 
