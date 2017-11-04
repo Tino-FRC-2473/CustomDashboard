bbab770565f9aacb843383bb7fc5481461071251
harmhe <he.harmony@gmail.com>
2017-11-04 15:55:03 -0700

    Worked on some stuff in GraphController

--- src/application/GraphController.java
+++ src/application/GraphController.java
@@ -5,22 +5,46 @@ import com.sun.javafx.geom.transform.BaseTransform;
 import com.sun.javafx.jmx.MXNodeAlgorithm;
 import com.sun.javafx.jmx.MXNodeAlgorithmContext;
 import com.sun.javafx.sg.prism.NGNode;
+
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 
 public class GraphController extends Node {
 
 	private BorderPane pane = new BorderPane();
 
 	private void setLayout() {
-		// add all of your stuff here
-		HBox hBox = new HBox();
-		Label label = new Label("test");
-		hBox.getChildren().add(label);
-		pane.setTop(hBox);
+		ScrollPane spR = new ScrollPane();
+		
+		Rectangle2D screenBalance = Screen.getPrimary().getVisualBounds(); //for getting screen dimensions
+		double screenWidth = screenBalance.getWidth();
+		double screenHeight = 650;
+		
+	    spR.setPrefSize(screenWidth / 2, screenHeight);
+	    spR.setContent(new Rectangle(screenWidth / 2, screenHeight * 2, Color.ALICEBLUE));
+	    pane.setRight(spR);
+	    
+	    ScrollPane spL = new ScrollPane();
+	    spL.setPrefSize(screenWidth / 2, screenHeight);
+	    spL.setContent(new Rectangle(screenWidth / 2, screenHeight * 2, Color.ALICEBLUE));
+	    pane.setLeft(spL);
+	    
+		XYChart.Series series = new XYChart.Series();
+		series.getData().add(new XYChart.Data());
+		final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+		LineChart<Number, Number> lc = new LineChart<Number, Number>(xAxis, yAxis);
 	}
 
 	public Pane getContent() {
