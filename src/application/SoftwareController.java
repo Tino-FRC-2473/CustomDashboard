75d22833c5562fd48560559a0c23d0b6fa9ca469
Krish Sankaran <ksankaran2107@gmail.com>
2017-11-06 18:09:35 -0800

    Added TableView to Software tab

--- src/application/SoftwareController.java
+++ src/application/SoftwareController.java
@@ -7,9 +7,12 @@ import com.sun.javafx.jmx.MXNodeAlgorithmContext;
 import com.sun.javafx.sg.prism.NGNode;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class SoftwareController extends Node {
 
@@ -18,9 +21,31 @@ public class SoftwareController extends Node {
 	private void setLayout() {
 		// add all of your stuff here
 		HBox hBox = new HBox();
-		Label label = new Label("test");
-		hBox.getChildren().add(label);
-		pane.setTop(hBox);
+		HBox inhBox = new HBox();
+		
+		TableView sensorTable = new TableView();
+		TableColumn sensorCol = new TableColumn("Sensors");
+		TableColumn sensorValCol = new TableColumn("Values");
+		sensorTable.setEditable(false);
+		sensorTable.getColumns().addAll(sensorCol, sensorValCol);
+		
+		TableView networkingTable = new TableView();
+		TableColumn netCol1 = new TableColumn("Networking!");
+		TableColumn netCol2 = new TableColumn("!gnikrowteN");
+		sensorTable.setEditable(false);
+		networkingTable.getColumns().addAll(netCol1, netCol2);
+		
+		TableView motorTable = new TableView();
+		TableColumn motorCol = new TableColumn("Motors");
+		TableColumn motorValCol = new TableColumn("Values");
+		sensorTable.setEditable(false);
+		motorTable.getColumns().addAll(motorCol, motorValCol);
+		
+		hBox.getChildren().add(sensorTable);
+		hBox.getChildren().add(inhBox);
+		inhBox.getChildren().add(networkingTable);
+		inhBox.getChildren().add(motorTable);
+		pane.setCenter(hBox);
 	}
 
 	public Pane getContent() {
