bfc41b305c21527cb9a99887f4166367e4c93607
Krish Sankaran <ksankaran2107@gmail.com>
2017-11-08 20:01:42 -0800

    added ValueNode and ValueNodeAnimation

--- src/application/ValueNode.java
+++ src/application/ValueNode.java
@@ -1,33 +1,22 @@
 package application;
 
-import javafx.animation.*;
-import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 
-//This class is used to display values, updated in real time.
-//Multiple ValueNode objects can be used to make a table.
-public class ValueNode extends AnimationTimer {
+public class ValueNode extends HBox {
 
-	//value: raw value that is tracked
-	Text value;
-	//name: name of value tracked
-	Text name;
-	//don't forget to check Text class API: https://docs.oracle.com/javafx/2/text/jfxpub-text.htm
+	private Label value;
+	private Label name;
 
-	public ValueNode(double x, double y, String nameText) { //need to find out way to track values
-		name = new Text(nameText);
-		name.setX(x);
-		name.setY(y);
-		value = new Text();
-		//value Text is offset to the right by 100, so that name and value don't overlap
-		name.setX(x + 100);
-		name.setY(y);
+	public ValueNode(String nameText) { //need to find out way to track values
+		name = new Label(nameText);
+		value = new Label("0");
+		this.getChildren().add(name);
+		this.getChildren().add(value);
 	}
 
-	@Override
-	public void handle(long arg0) {
-		//value updated constantly within this method
-		//long is measured in nanoseconds (1e^-9 seconds)
-		//use modulus to configure rate at which value is updated
-	}
+	public void setValue(double n) {
+		value.setText(n+"");
+	}	
 
 }
